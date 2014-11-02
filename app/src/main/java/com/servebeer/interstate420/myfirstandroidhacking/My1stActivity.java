32dfcf440f15d7a33f03c9afc4a05941495bf276
Timothy Pratte <interstate420@gmail.com>
2014-11-02 00:50:08 -0500

    Log.i TAG msgs for activity changes

--- app/src/main/java/com/servebeer/interstate420/myfirstandroidhacking/My1stActivity.java
+++ app/src/main/java/com/servebeer/interstate420/myfirstandroidhacking/My1stActivity.java
@@ -4,16 +4,68 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
 
 
 public class My1stActivity extends Activity {
+
+    private static final String TAG = "com.servebeer.interstate420.myfirstandroidhacking";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my1st);
+        Log.i( TAG, "onCreate");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i (TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i ( TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG,"onSaveInstanceState:");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
